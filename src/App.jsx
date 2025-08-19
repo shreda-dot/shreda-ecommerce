03bf81f86e3f2a4415745a4ba1f027f149356544
peter <ezinwa.ugochukwu@gmail.com>
2025-08-19 16:18:19 +0100

    moved the home pages into react

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Testing from './testing.jsx'
+import HomePage from './ecommerce-project-main/Pages/HomePage.jsx'
 
 const App = () => {
   return (
     <>
+    <HomePage />
 <Testing />
     </>
   )
